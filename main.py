b65c3cb4dd40434f040d248394c641b8e8195a93
Chenghong Li <lichenghongnk@126.com>
2019-01-19 15:58:09 +0800

    Webp image to png image; Fetch MRI links

--- main.py
+++ main.py
@@ -1,20 +1,24 @@
 import requests as rs
+import json
+import os
 
 
 class MangaRock:
     def __init__(self):
         self.magicNum = 0x65
 
-    def mri2webp(self, mriFile, webpFile):
+    def mri2webp(self, mriFile, webpFile=None):
+        print('MRI file to webp image...')
+        if webpFile is None:
+            webpFile = mriFile.replace('.mri', '.webp')
+
         with open(mriFile, 'rb') as fin:
             content = fin.read()
         size = len(content) + 7
-        print(len(content))
+        print('MRI file size:', len(content))
         contentArray = bytearray(content)
         sizeHex = size.to_bytes(4, byteorder='little')
-        print(sizeHex)
-        sizeHexStr = 'c6b1 0800'
-        newContentArray = bytearray.fromhex('5249 4646') # + sizeHexStr + ' 5745 4250 5650 3820')
+        newContentArray = bytearray.fromhex('5249 4646')
         newContentArray += bytearray(sizeHex)
         newContentArray += bytearray.fromhex('5745 4250 5650 38')
 
@@ -22,15 +26,74 @@ class MangaRock:
             contentArray[i] ^= self.magicNum
         newContentArray += contentArray
 
-        print(len(newContentArray))
+        print('WEBP file size', len(newContentArray))
         with open(webpFile, 'wb') as fout:
             fout.write(newContentArray)
+        print('Done. WEBP Image at', webpFile, '\n')
 
+    def webp2png(self, webpFile, pngFile=None):
+        print('WEBP image to png image...')
+        try:
+            from PIL import Image
+        except ImportError as e:
+            print('Pillow must be installed to convert webp to png')
+            raise e
+        if pngFile is None:
+            pngFile = webpFile.replace('.webp', '.png')
+        im = Image.open(webpFile).convert("RGBA")
+        im.save(pngFile, "png")
+        print('Done. PNG image at', pngFile, '\n')
 
+    def downloadMRI(self, url, filepath=None):
+        print('Fetch MRI file...')
+        r = rs.get(url, stream=True)
 
+        if filepath is None:
+            filepath = url[url.rfind('/')+1:]
+
+        with open(filepath, "wb") as pdf:
+            for chunk in r.iter_content(chunk_size=1024):
+
+                # writing one chunk at a time to pdf file
+                if chunk:
+                    pdf.write(chunk)
+        print('Done. MRI file at', filepath, '\n')
+
+    def getMRIListByChapter(self, chapterId):
+        print('Get MRI List by chapter ID...')
+        r = rs.get('https://api.mangarockhd.com/query/web401/pagesv2?oid=mrs-chapter-%s&country=Japan' % chapterId)
+        data = json.loads(r.content)['data']
+        mriList = []
+        for item in data:
+            mriList.append(item['url'])
+        print('Done.', len(data), 'items obtained.\n')
+        return mriList
+
+    def getComicByChapter(self, chapterId, folder='./', to_png=True, delete_tempfile=True):
+        print('Get comic of chapter %s......' % chapterId)
+        mriList = self.getMRIListByChapter(chapterId)
+        for i, mri in enumerate(mriList):
+            print('Process image %d / %d' % (i, len(mriList)))
+            mriFile = os.path.join(folder, 'ch%s_%d.mri' % (chapterId, i))
+            webpFile = os.path.join(folder, 'ch%s_%d.webp' % (chapterId, i))
+            pngFile = os.path.join(folder, 'ch%s_%d.png' % (chapterId, i))
+            self.downloadMRI(mri, mriFile)
+            self.mri2webp(mriFile, webpFile)
+            if to_png:
+                self.webp2png(webpFile, pngFile)
+            if delete_tempfile:
+                print('Remove temporary files...')
+                os.remove(mriFile)
+                if to_png:
+                    os.remove(webpFile)
+        print('Get comic of chapter %s finished.' % chapterId)
 
 
 
 if __name__ == '__main__':
     mr = MangaRock()
-    mr.mri2webp('./tB.cLwx5xUK.mri', './1.webp')
+    mriList = mr.getMRIListByChapter('100399152')
+
+    # mr.downloadMRI('https://f01.mrcdn.info/file/mrfiles/j/1/a/e/tB.cLwx5xUK.mri') # mriList[2]
+    # mr.mri2webp('./tB.cLwx5xUK.mri', './tB.cLwx5xUK.webp')
+    # mr.webp2png('./tB.cLwx5xUK.webp')
